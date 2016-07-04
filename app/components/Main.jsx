58ac9c3f053181836838ecbf31f1ccfeb1f3ad9e
Nimrod Bahar <nimrodbahar@gmail.com>
2016-07-04 10:48:10 +0200

    Add CountdownFom component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,7 @@ var Main = (props) => {
     <div>
       <Navigation/>
       <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
+        <div className="column small-centered medium-6 large-4">
           {props.children}
         </div>
       </div>
